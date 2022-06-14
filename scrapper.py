c71dc3895d003cd89b8cfefe4c4bd718c3281336
bltomlin <>
2022-06-13 18:40:49 -0700

    This updated code evaluates a webpage to see if it is a movie page from IMBD. If it is it will collect the data from the webpage and append the movie title and description to a dictionary. Once the data is appended, it will print the dictionary.

--- scrapper.py
+++ scrapper.py
@@ -1,14 +1,21 @@
+"""
+Grabs a URL and evaluates it to see if it has movie data from IMBD.
+"""
+
 import requests
 
+from bs4 import BeautifulSoup
+
 url = input()
-response = requests.get(url)
-text = response.json()
-content = False
-for i in text:
-    if i == 'content':
-        content = True
-        phrase = text[i]
-if content:
-    print(phrase)
-else:
-    print("Invalid quote resource!")
+response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+soup = BeautifulSoup(response.content, 'html.parser')
+h1 = soup.find('h1')
+if h1 is None:
+    print('Invalid movie page!')
+    quit()
+span = soup.find('span', {'data-testid': 'plot-l'})
+if span is None:
+    print('Invalid movie page!')
+    quit()
+dictionary = {'title': h1.text, 'description': span.text}
+print(dictionary)
