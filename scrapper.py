159bbf59c0b9048eb0cf3ab96f779af6563d7db0
bltomlin <>
2022-06-13 18:45:12 -0700

    Updated with comments to help readers understand how the code works.

--- scrapper.py
+++ scrapper.py
@@ -6,16 +6,17 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url = input()
-response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-soup = BeautifulSoup(response.content, 'html.parser')
-h1 = soup.find('h1')
-if h1 is None:
+url = input()  # Grabs URL from user input
+response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})  # Requested data comes in English
+soup = BeautifulSoup(response.content, 'html.parser')  # Tool to scrape HTML off webpage
+h1 = soup.find('h1')  # Looks for the title of the movie
+if h1 is None:  # Evaluates if there is a title
     print('Invalid movie page!')
     quit()
-span = soup.find('span', {'data-testid': 'plot-l'})
-if span is None:
+span = soup.find('span', {'data-testid': 'plot-l'})  # Looks for the description of the movie
+if span is None:  # Evaluates if there is a description
     print('Invalid movie page!')
     quit()
-dictionary = {'title': h1.text, 'description': span.text}
-print(dictionary)
+dictionary = {'title': h1.text, 'description': span.text}  # parses data to dictionary
+print(dictionary)  # prints resulting dictionary
+
