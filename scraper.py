52dc960d962cef887be8308b5093a41336524d48
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-06-27 23:48:12 -0700

    update scrapper.py
    Using beautiful soup library to scrape URL and write HTML to txt file.

---
+++ scraper.py
@@ -0,0 +1,17 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+url = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3'  # Grabs URL from user input
+response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})  # Requested data comes in English
+soup = BeautifulSoup(response.content, 'html.parser')  # Tool to scrape HTML off webpage
+link = soup.find_all('article')
+for i in link:
+    if i.find_next('span', class_="c-meta__type").text == 'News':
+        hrefs = (i.find("a").get("href"))
+        web_request = requests.get('https://www.nature.com' + hrefs).content
+        file = open('source.txt', 'wb')
+        file.write(web_request)
+        file.close()
+        print('Content saved.')
+print(soup.find('p').text)
